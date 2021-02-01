44c2f4938ccae330b18fe6df7e11f8c96856cd41
Esteban R. Briones Sanjuan <stephenelf@gmail.com>
2021-02-01 18:58:07 +0100

    added path

--- vars/CloneiOSApp.groovy
+++ vars/CloneiOSApp.groovy
@@ -5,7 +5,7 @@ import com.stephenelf.jenkins.iOSUtils
 def call(def String path,def String origin_target, def String new_target,def destination,def assetsUrl, def String googleServicesUrl,
                 String slug_race,String primary, String accent){
     iOSUtils.copytarget(path,origin_target,new_target,destination)
-    Utils.downloadAssets(assetsUrl,"assets.zip","${destination}${new_target}/${new_target}.xcassets")
+    Utils.downloadAssets(assetsUrl,"${path}/assets.zip","${destination}${new_target}/${new_target}.xcassets")
     Utils.downloadGoogleServices(googleServicesUrl,"GoogleService-Info.plist","${destination}${new_target}")
     iOSUtils.updateSMConstants(new File("${destination}${new_target}/SMConstants.swift"),slug_race,
             primary,primary)
